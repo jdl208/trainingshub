9814cd112309bd78337a95135945d0ddac2a38c7
Johan de Leeuw <johandeleeuw@gmail.com>
2020-01-12 21:14:07 +0100

    created listview

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Courses
 
-# Create your views here.
+
+class CourseListView(ListView):
+    model = Courses
+    
+
