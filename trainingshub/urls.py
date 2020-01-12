892e027fe19c7f158632e0f68e31c795fe8bb987
Johan de Leeuw <johandeleeuw@gmail.com>
2020-01-12 21:13:34 +0100

    Added courses path

--- trainingshub/urls.py
+++ trainingshub/urls.py
@@ -20,7 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('users.urls')),
+    path('users', include('users.urls')),
+    path('', include('courses.urls')),
 ]
 
 if settings.DEBUG:
