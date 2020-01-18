ebc7894573a800be773285bf596ce04b1e2ba760
Johan de Leeuw <johandeleeuw@gmail.com>
2020-01-18 16:38:28 +0100

    added list path

--- users/urls.py
+++ users/urls.py
@@ -4,8 +4,9 @@ from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-    path('register', users_views.register, name='register'),
-    path('login', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout', users_views.logout, name='logout'),
-    path('', users_views.profile, name='home'),
+    path('list/', users_views.userlist, name='user_list'),
+    path('register/', users_views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', users_views.logout, name='logout'),
+    path('', users_views.profile, name='profile'),
 ]
