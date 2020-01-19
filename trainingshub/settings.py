ecb4ff5233c922359ca5d6cfc35cc9022dc60170
Johan de Leeuw <johandeleeuw@gmail.com>
2020-01-19 17:01:32 +0100

    Created view to landingpage

--- trainingshub/settings.py
+++ trainingshub/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
     'courses.apps.CoursesConfig',
+    'home.apps.HomeConfig',
     'crispy_forms',
 ]
 
--- trainingshub/urls.py
+++ trainingshub/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('home.urls')),
     path('users/', include('users.urls')),
     path('courses/', include('courses.urls')),
 ]
