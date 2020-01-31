a17b048aa1c3de914aa44bf6a13a1d4e573731d3
Johan de Leeuw <johandeleeuw@gmail.com>
2020-01-31 22:17:54 +0100

    Made first draft of the signup model

--- signups/models.py
+++ signups/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from courses.models import Courses
+from datetime import timezone
 
-# Create your models here.
+class Signups(models.Model):
+    course = models.OneToOneField(Courses, on_delete=models.CASCADE)
+    student = models.OneToOneField(User, on_delete=models.CASCADE)
+    PAYMENT_CHOICES = [
+        ('ID', 'Ideal'),
+        ('CC', 'Credit card'),
+        ('PI', 'Pin'),
+    ]
+    payment_method = models.CharField(max_length=2, choices=PAYMENT_CHOICES, default='ID')
+    paid = models.BooleanField()
+    signup_date = models.DateField(default=timezone.now)
+    notes = models.TextField()
+    cancelled = models.BooleanField()
