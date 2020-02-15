05403fd13b3ec2bbcd0f3999933ac46ce48ccdc7
Johan de Leeuw <johandeleeuw@gmail.com>
2020-02-15 17:58:33 +0100

    Added signed up courses to profile view

--- users/views.py
+++ users/views.py
@@ -1,56 +1,70 @@
-from django.shortcuts import render, redirect, reverse
-from django.contrib.auth.forms import UserCreationForm
+from datetime import datetime
+
 from django.contrib import auth, messages
-from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib.auth.models import User
-from django.db.models import Q
 from django.core.paginator import Paginator
+from django.db.models import Q
+from django.shortcuts import redirect, render, reverse
+
+from signups.models import Signup
+
+from .forms import ProfileUpdateForm, UserRegisterForm, UserUpdateForm
+
 
 def register(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            user = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {user}!')
-            return redirect('login')
+            user = form.cleaned_data.get("username")
+            messages.success(request, f"Account created for {user}!")
+            return redirect("login")
     else:
         form = UserRegisterForm()
-    return render(request, 'users/register.html', {'form': form})
+    return render(request, "users/register.html", {"form": form})
+
 
 @login_required
 def logout(request):
     """ Log the user out """
     auth.logout(request)
     messages.success(request, "You have successfully been logged out!")
-    return redirect(reverse('login'))
+    return redirect(reverse("login"))
 
 
 @login_required
 def account(request):
-    user = request.user
-    return render(request, 'users/user_detail.html', {'user': user})
+    context = {
+        "upcoming": Signup.objects.filter(
+            registrant=request.user, course__date__gte=datetime.today()
+        ),
+        "past": Signup.objects.filter(
+            registrant=request.user, course__date__lte=datetime.today()
+        ),
+        "user": request.user,
+    }
+    return render(request, "users/user_detail.html", context)
 
 
 @login_required
 def profile(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         u_form = UserUpdateForm(request.POST, instance=request.user)
-        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        p_form = ProfileUpdateForm(
+            request.POST, request.FILES, instance=request.user.profile
+        )
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
-            messages.success(request, 'Your account has been updated!')
-            return redirect('account')
-    else:
-        u_form = UserUpdateForm(instance=request.user)
-        p_form = ProfileUpdateForm(instance=request.user.profile)
+            messages.success(request, "Your account has been updated!")
+            return redirect("account")
     context = {
-        'u_form': u_form,
-        'p_form': p_form
+        "u_form": UserUpdateForm(instance=request.user),
+        "p_form": ProfileUpdateForm(instance=request.user.profile),
     }
-    return render(request, 'users/profile.html', context)
+    return render(request, "users/profile.html", context)
+
 
 def is_staff(user):
     """
@@ -58,31 +72,32 @@ def is_staff(user):
     """
     return user.is_staff
 
-@user_passes_test(is_staff, login_url='home')
+
+@user_passes_test(is_staff, login_url="home")
 def userlist(request):
     userlist = User.objects.all()
     p = Paginator(userlist, 10)
-    page = request.GET.get('page')
+    page = request.GET.get("page")
     users = p.get_page(page)
-    return render(request, 'users/users_list.html', {'users': users})
+    return render(request, "users/users_list.html", {"users": users})
 
 
-@user_passes_test(is_staff, login_url='home')
+@user_passes_test(is_staff, login_url="home")
 def user_detail(request, id):
     user = User.objects.get(pk=id)
-    return render(request, 'users/user_detail.html', {'user':user})
-    
+    return render(request, "users/user_detail.html", {"user": user})
+
 
 def user_search(request):
     """
     Search user by username/email, first name or last name
     """
     userlist = User.objects.filter(
-        Q(username__icontains=request.GET['q']) |
-        Q(first_name__icontains=request.GET['q']) |
-        Q(last_name__icontains=request.GET['q'])
-        )
+        Q(username__icontains=request.GET["q"])
+        | Q(first_name__icontains=request.GET["q"])
+        | Q(last_name__icontains=request.GET["q"])
+    )
     p = Paginator(userlist, 10)
-    page = request.GET.get('page')
+    page = request.GET.get("page")
     users = p.get_page(page)
-    return render(request, 'users/users_list.html', {'users': users })
+    return render(request, "users/users_list.html", {"users": users})
