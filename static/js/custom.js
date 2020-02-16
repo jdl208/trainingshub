5284ad1b2300a57a0886888297efcd815fb375df
Johan de Leeuw <johandeleeuw@gmail.com>
2020-02-16 15:43:00 +0100

    Styled the user_detail.html with courses signed up for

--- static/js/custom.js
+++ static/js/custom.js
@@ -17,4 +17,21 @@ $(document).ready(function() {
     $(".clickable-row").click(function() {
         window.location = $(this).data("href");
     });
+    // change active tab on account page of user and
+    $("#course-nav a").on("click", function(e) {
+        e.preventDefault();
+        $(this).tab("show");
+    });
+    $("#upcoming").on("click", function() {
+        $("#upcoming-courses").removeClass("d-none");
+        $("#past-courses").addClass("d-none");
+        $("#past").addClass("text-white");
+        $("#upcoming").removeClass("text-white");
+    });
+    $("#past").on("click", function() {
+        $("#past-courses").removeClass("d-none");
+        $("#upcoming-courses").addClass("d-none");
+        $("#upcoming").addClass("text-white");
+        $("#past").removeClass("text-white");
+    });
 });
