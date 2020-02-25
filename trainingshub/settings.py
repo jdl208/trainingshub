be9d71f28000ce650ac9c1cd30d0054d011cf870
Johan de Leeuw <johandeleeuw@gmail.com>
2020-02-25 20:33:32 +0100

    Corrected Typo

--- trainingshub/settings.py
+++ trainingshub/settings.py
@@ -93,7 +93,7 @@ if DEBUG:
         }
     }
 else:
-    DATABASES = {"default": {dj_database_url.parse(os, getenv("DATABASE_URL"))}}
+    DATABASES = {"default": {dj_database_url.parse(os.getenv("DATABASE_URL"))}}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -102,9 +102,9 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
-    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",},
-    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",},
-    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
 
 
