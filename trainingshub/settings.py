159a618dd802e8d1a7e1a779ce581c0660bf4fe4
Johan de Leeuw <johandeleeuw@gmail.com>
2020-02-25 21:36:23 +0100

    commented out test database

--- trainingshub/settings.py
+++ trainingshub/settings.py
@@ -85,19 +85,20 @@ WSGI_APPLICATION = "trainingshub.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-if DEBUG:
-    # DEVELOPEMENT DATABASE
-    print("TEST DATABASE RUNNING")
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-        }
-    }
-else:
-    # PRODUCTION DATABASE
-    print("PRODUCTION DATABASE RUNNING")
-    DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
+# if DEBUG:
+#     # DEVELOPEMENT DATABASE
+#     print("TEST DATABASE RUNNING")
+#     DATABASES = {
+#         "default": {
+#             "ENGINE": "django.db.backends.sqlite3",
+#             "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+#         }
+#     }
+# else:
+
+# PRODUCTION DATABASE
+print("PRODUCTION DATABASE RUNNING")
+DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
