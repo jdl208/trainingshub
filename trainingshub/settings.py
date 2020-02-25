5cfc1c814c0426dd54d61959a7cd503c34dd03f6
Johan de Leeuw <johandeleeuw@gmail.com>
2020-02-25 22:15:52 +0100

    removed line with print function

--- trainingshub/settings.py
+++ trainingshub/settings.py
@@ -91,7 +91,6 @@ if "DATABASE_URL" in os.environ:
     DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
 else:
     # DEVELOPEMENT DATABASE
-    print("TEST DATABASE RUNNING")
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
