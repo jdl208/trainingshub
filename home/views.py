2df7287d40a02d50e680c027425637c40072541b
Johan de Leeuw <johandeleeuw@gmail.com>
2020-02-29 10:13:47 +0100

    Added latest added course to frontpage

--- home/views.py
+++ home/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from courses.models import Courses
+from signups.models import Signup
 
 
 def home(request):
-    return render(request, "home/home.html")
+    if request.user.is_authenticated:
+        signedup_list = Signup.objects.filter(registrant=request.user).values_list("course_id", flat=True)
+    else:
+        signedup_list = []
+    for i in signedup_list:
+        print(signedup_list)
+    context = {"course": Courses.objects.last(), "signedup": signedup_list}
+    return render(request, "home/home.html", context)
