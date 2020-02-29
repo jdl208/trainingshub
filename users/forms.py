18b256e5414ad861cf58e94649f1e87950b2a0ab
Johan de Leeuw <johandeleeuw@gmail.com>
2020-02-29 12:12:56 +0100

    made code pep8 compliant

--- users/forms.py
+++ users/forms.py
@@ -3,35 +3,38 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Profile
 
+
 class UserRegisterForm(UserCreationForm):
     username = forms.EmailField(max_length=75, label="Email")
 
     class Meta:
         model = User
-        fields = ['username', 'password1', 'password2']
+        fields = ["username", "password1", "password2"]
 
     def save(self, commit=True):
         user = super().save(False)
         user.email = user.username
         user = super().save()
         return user
+
 
 class UserUpdateForm(forms.ModelForm):
     username = forms.EmailField(max_length=75, label="Email")
 
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name']
-
+        fields = ["username", "first_name", "last_name"]
 
     def save(self, commit=True):
         user = super().save(False)
         user.email = user.username
         user = super().save()
         return user
-    
+
+
 class ProfileUpdateForm(forms.ModelForm):
     image = forms.ImageField()
+
     class Meta:
         model = Profile
-        fields = ['company_name', 'address', 'postcode', 'city', 'image', 'rtgs_nr']
+        fields = ["company_name", "address", "postcode", "city", "image", "rtgs_nr"]
