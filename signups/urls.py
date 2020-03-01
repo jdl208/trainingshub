4ab80a3131ad33a263d807f9c3472a30dc413bb8
Johan de Leeuw <johandeleeuw@gmail.com>
2020-03-01 09:40:55 +0100

    added signup detail view

--- signups/urls.py
+++ signups/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path("signuplist/coming/", views.signup_list, name="signuplist"),
     path("signuplist/completed/", views.signup_list_completed, name="signuplist-completed"),
+    path("signup-detail/<id>/", views.signup_detail, name="signup-detail"),
     path("<id>/", views.signup, name="signup"),
     path("<id>/checkout/", views.checkout, name="checkout"),
 ]
