9d41312744c25ec7178234fd41b2c74c92185c0d
Johan de Leeuw <johandeleeuw@gmail.com>
2020-03-01 09:41:19 +0100

    removed 2 models fields

--- users/models.py
+++ users/models.py
@@ -4,8 +4,6 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
     company_name = models.CharField(max_length=100, blank=True)
     address = models.CharField(max_length=50)
     postcode = models.CharField(max_length=6)
