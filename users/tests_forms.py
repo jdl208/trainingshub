fc64e5d4c1da93af9bf0d774968ea2e4b793e637
Johan de Leeuw <johandeleeuw@gmail.com>
2020-03-02 20:01:32 +0100

    aded test forms file

---
+++ users/tests_forms.py
@@ -0,0 +1,27 @@
+from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+
+class TestUserViews(TestCase):
+    def test_register_GET(self):
+        response = self.client.get(reverse("register"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "users/register.html")
+
+    def test_register_POST_add_new_user(self):
+        response = self.client.post(
+            reverse("register"), {"username": "test@test.test", "password1": "testing321", "password2": "testing321"}
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(User.objects.first().username, "test@test.test")
+
+    def test_logout(self):
+        # self.client.login(username="foo", password="bar")
+        response = self.client.get(reverse("logout"))
+        self.assertEqual(response.status_code, 302)
+
+    # def test_account(self):
+    #     # self.client.login(username="foo", password="bar")
+    #     response = self.client.get(reverse("account"))
+    #     self.assertEqual(response.status_code, 302)
