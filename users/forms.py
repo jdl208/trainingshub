fd7c2b07763b78b95090852d36bb9a3922f58dbc
Johan de Leeuw <johandeleeuw@gmail.com>
2020-03-04 21:51:38 +0100

    Added default names because of postrgress error on heroku

--- users/forms.py
+++ users/forms.py
@@ -14,6 +14,8 @@ class UserRegisterForm(UserCreationForm):
     def save(self, commit=True):
         user = super().save(False)
         user.email = user.username
+        user.first_name = "First name"
+        user.last_name = "Last name"
         user = super().save()
         return user
 
