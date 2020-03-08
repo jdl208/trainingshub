7822c54c382ba25fe0d6e818cf7c1c00b2c8282a
Johan de Leeuw <johandeleeuw@gmail.com>
2020-03-08 21:49:14 +0100

    removed double test from test names

--- courses/tests_models.py
+++ courses/tests_models.py
@@ -25,11 +25,11 @@ class TestLocationModel(TestCase):
             description="This is a test",
         )
 
-    def test_test_location_as_a_string(self):
+    def test_location_as_a_string(self):
         self.assertEqual(str(self.loc), "Test - Amsterdam")
 
-    def test_test_course_type_as_a_string(self):
+    def test_course_type_as_a_string(self):
         self.assertEqual(str(self.ct), "Training")
 
-    def test_test_course_as_a_string(self):
+    def test_course_as_a_string(self):
         self.assertEqual(str(self.course), "2021-01-01 - Test")
